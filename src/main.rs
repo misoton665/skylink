8b5c36724305d7742d399ec60f1d27657ccef738
misoton665 <misoton998@gmail.com>
2016-10-25 16:04:05 +0900

    Remove extra codes in main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,9 @@ extern crate skylink;
 
 use std::env;
 
-use skylink::link::Link;
 use skylink::commands::command;
 
 fn main() {
-  let l = Link::new("pom", "hom", false);
-  let k = Link::new("pom", "hoge", false);
-
-  println!("l: {}", &l.id);
-
-  println!("k: {}", &k.id);
-
-  println!("l == k: {}", l == k);
-
-  let list = vec![1,2,3,4,5];
-
-  let p = list.iter().fold(Vec::new(), |mut acc, x| {acc.push(x); acc});
-
-  println!("p: {}", &p[0]);
 
   let result = command::runCommand(env::args());
 
