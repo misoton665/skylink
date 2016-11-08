322073e1630047d4fea93ae39c36395b0bc00d2b
misoton665 <misoton998@gmail.com>
2016-11-08 18:01:10 +0900

    Add confirm in init command

--- src/commands/init_command.rs
+++ src/commands/init_command.rs
@@ -1,6 +1,30 @@
+use std::io;
+
 use commands::command::*;
 
 pub fn run_init_command() -> CommandResult {
-  println!("Init command!");
+  println!("Init command will clean configure file, and make new empty one.\nDo you really run init command? (y/n): ");
+
+  loop {
+    let mut answer = String::new();
+
+    match io::stdin().read_line(&mut answer) {
+      Ok(_) => (),
+      Err(_) => {
+        println!("IO error.");
+        continue;
+      }
+    };
+
+    let answer = answer.trim();
+
+    if answer == "n" {
+      return command_result!(false);
+    } else if answer == "y" {
+      break;
+    } else {
+      println!("Please enter the character \'y\' or \'n\'");
+    }
+  }
   command_result!(true)
 }
